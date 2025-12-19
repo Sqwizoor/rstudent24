b4007ca9876065f3b8ada9f0162bf4ee913717cb
alex banele <alexsouthflow@gmail.com>
2025-12-19 12:51:12 +0200

    chore: remove prisma.config.ts to avoid parse errors on older prisma

--- prisma.config.ts
+++
@@ -1,5 +0,0 @@
-// Delegate to the CJS config so build servers that can't parse TypeScript will still succeed.
-// Keeping this file minimal prevents Prisma from failing when it tries to parse .ts files on hosts
-// that don't transpile TypeScript.
-module.exports = require('./prisma.config.cjs');
-
